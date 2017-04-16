29ed8e3c807a519bbaa915e06b985a64161ef05a
Andreas Larsen <andreas@larsenwork.com>
2017-04-16 09:15:46 +0200

    Cleanup and Linting WIP

--- helpers.js
+++ helpers.js
@@ -1,13 +1,13 @@
 /**
  * Require constants
  */
-const Color         = require('color');
-const round10       = require('round10').round10;
-const easeInSine    = require('eases/sine-in');
-const easeOutSine   = require('eases/sine-out');
+const Color = require('color');
+const round10 = require('round10').round10;
+const easeInSine = require('eases/sine-in');
+const easeOutSine = require('eases/sine-out');
 const easeInOutSine = require('eases/sine-in-out');
-const easeInQuad    = require('eases/quad-in');
-const easeOutQuad   = require('eases/quad-out');
+const easeInQuad = require('eases/quad-in');
+const easeOutQuad = require('eases/quad-out');
 const easeInOutQuad = require('eases/quad-in-out');
 
 /**
@@ -27,23 +27,16 @@ const scrimCoordinates = {
   0.94: '80.36%',
   0.97: '87.18%',
   0.99: '93.73%'
-}
-
-/**
- * Calculate the color stops based on start+stopColor in an array and easingType
- */
-exports.getColorStops = function(colors, easingType, delta, alphaDecimals) {
-  colors = transparentFix(colors);
-  let gradientCoordinates = getCoordinates(easingType, delta);
-  let colorStops = '';
-  for (let ammount in gradientCoordinates) {
-    let color = Color(colors[1]).mix(Color(colors[0]), ammount);
-    color = roundHslAlpha(color.hsl().string(), alphaDecimals);
-    colorStops += `, ${color} ${gradientCoordinates[ammount]}`;
-  }
-  colorStops += `, ${colors[1]} 100%`;
-  return colorStops;
 };
+const supportedGradients = [
+  'ease-in-sine-gradient',
+  'ease-out-sine-gradient',
+  'ease-in-out-sine-gradient',
+  'ease-in-quad-gradient',
+  'ease-out-quad-gradient',
+  'ease-in-out-quad-gradient',
+  'scrim-gradient'
+];
 
 /**
  * If a color is transparent then convert it to a transparent of the sibling
@@ -53,13 +46,50 @@ function transparentFix(colors) {
     ? Color(colors[Math.abs(i - 1)]).alpha(0).hsl().string()
     : color
   );
-};
+}
+
+/**
+ * Test if new coordinate is far enough away from old coordinate
+ */
+function isFarEnough(x, y, xOld, yOld, delta) {
+  return Math.sqrt(((x - xOld) ** 2) + ((y - yOld) ** 2)) > delta;
+}
+
+/**
+ * Convert decimal number to percentage string
+ */
+function getPercentage(number) {
+  return `${round10(number * 100, -1)}%`;
+}
+
+/**
+ * Easing functions switcheroo
+ */
+function ease(x, type) {
+  switch (type) {
+    case 'ease-in-sine-gradient':
+      return easeInSine(x);
+    case 'ease-out-sine-gradient':
+      return easeOutSine(x);
+    case 'ease-in-out-sine-gradient':
+      return easeInOutSine(x);
+    case 'ease-in-quad-gradient':
+      return easeInQuad(x);
+    case 'ease-out-quad-gradient':
+      return easeOutQuad(x);
+    case 'ease-in-out-quad-gradient':
+      return easeInOutQuad(x);
+    default:
+      console.log(`Sorry, easing gradient does not support ${type}.`);
+      return null;
+  }
+}
 
 /**
  * Get coordinates based on easing function.
  * Delta checks ensures there's roughly the same distance between coordinates.
  */
-function getCoordinates(easingFunction, delta) {
+function getCoordinates(easingFunction, precision) {
   if (easingFunction === 'scrim-gradient') return scrimCoordinates;
 
   const yIncrements = 0.001;
@@ -72,6 +102,7 @@ function getCoordinates(easingFunction, delta) {
   let xOld = 0;
   let yOld = 0;
   let firstTime = true;
+  let delta = precision;
 
   while (firstTime || !isFarEnough(1, 1, xOld, yOld, delta - deltaTolerance)) {
     if (firstTime) {
@@ -81,7 +112,7 @@ function getCoordinates(easingFunction, delta) {
       y = 0;
       xOld = 0;
       yOld = 0;
-      delta = delta - deltaAdjust;
+      delta -= deltaAdjust;
       coordinates = {};
     }
     while (y <= 1) {
@@ -96,56 +127,14 @@ function getCoordinates(easingFunction, delta) {
     }
   }
   return coordinates;
-};
-
-/**
- * Easing functions switcheroo
- */
-function ease(x, type) {
-  switch (type) {
-    case 'ease-in-sine-gradient':
-      return easeInSine(x);
-      break;
-    case 'ease-out-sine-gradient':
-      return easeOutSine(x);
-      break;
-    case 'ease-in-out-sine-gradient':
-      return easeInOutSine(x);
-      break;
-    case 'ease-in-quad-gradient':
-      return easeInQuad(x);
-      break;
-    case 'ease-out-quad-gradient':
-      return easeOutQuad(x);
-      break;
-    case 'ease-in-out-quad-gradient':
-      return easeInOutQuad(x);
-      break;
-    default:
-      console.log(`Sorry, easing gradient does not support ${type}.`);
-  }
-};
-
-/**
- * Convert decimal number to percentage string
- */
-function getPercentage(number) {
-  return round10(number * 100, -1) + '%';
-};
-
-/**
- * Test if new coordinate is far enough away from old coordinate
- */
-function isFarEnough(x, y, xOld, yOld, delta) {
-  return Math.sqrt((x - xOld) ** 2 + (y - yOld) ** 2) > delta;
-};
+}
 
 /**
  * Round alpha in hsl colors to alphaDecimals
  */
 function roundHslAlpha(color, alphaDecimals) {
-  let prefix = color.substring(0, color.indexOf('('));
-  let values = color
+  const prefix = color.substring(0, color.indexOf('('));
+  const values = color
     .substring(color.indexOf('(') + 1, color.indexOf(')'))
     .split(',')
     .map(string => string.indexOf('%') === -1
@@ -153,4 +142,61 @@ function roundHslAlpha(color, alphaDecimals) {
       : string.trim()
     );
   return `${prefix}(${values.join(', ')})`;
+}
+
+/**
+ * Calculate the color stops based on start+stopColor in an array and easingType
+ */
+exports.getColorStops = function getColorStops(
+  colors,
+  easingType,
+  delta,
+  alphaDecimals
+) {
+  const fixedColors = transparentFix(colors);
+  const gradientCoordinates = getCoordinates(easingType, delta);
+  let colorStops = '';
+  Object.keys(gradientCoordinates).forEach((ammount) => {
+    // https://github.com/Qix-/color
+    let color = Color(fixedColors[1]).mix(Color(fixedColors[0]), ammount);
+    color = color.hsl().string();
+    color = roundHslAlpha(color, alphaDecimals);
+    colorStops += `${color} ${gradientCoordinates[ammount]}, `;
+  });
+  colorStops += `${colors[1]} 100%`;
+  return colorStops;
+};
+
+
+/**
+ * Check if a string matches one of the supported gradients
+ */
+exports.isEasingGradient = function isEasingGradient(str) {
+  return new RegExp(supportedGradients.join('|')).test(str);
+};
+
+/**
+ * Convert a function object to a word object
+ */
+exports.functionToWord = function functionToWord(obj) {
+  const object = obj;
+  const array = [];
+  // TODO: Make eslint happy
+  /* eslint-disable no-restricted-syntax */
+  for (const objNode of object.nodes) {
+    if (objNode.type === 'word') {
+      array.push(objNode.value);
+    }
+  }
+  /* eslint-enable no-restricted-syntax */
+  object.value = `${obj.value}(${array.join()})`;
+  object.type = 'word';
+  return object;
+};
+
+/**
+ * Test if two colors are identical
+ */
+exports.isSameColor = function isSameColor(colorA, colorB) {
+  return Color(colorA).hsl().string() === Color(colorB).hsl().string();
 };
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * Require constants
  */
-const Color       = require('color');
-const helpers     = require('./helpers.js');
-const postcss     = require('postcss');
+const Color = require('color');
+const helpers = require('./helpers.js');
+const postcss = require('postcss');
 const valueParser = require('postcss-value-parser');
 
 /**
@@ -12,65 +12,49 @@ const valueParser = require('postcss-value-parser');
 const defaultPrecision = 0.1;
 const defaultAlphaDecimals = 3;
 const errorPrefix = 'Error[postcss-easing-gradients]:';
-const supportedGradients = [
-  'ease-in-sine-gradient',
-  'ease-out-sine-gradient',
-  'ease-in-out-sine-gradient',
-  'ease-in-quad-gradient',
-  'ease-out-quad-gradient',
-  'ease-in-out-quad-gradient',
-  'scrim-gradient'
-];
 
 /**
  * The easing gradient function is a postcss plugin which supports the above
  * mentioned gradient types.
  */
-module.exports =
-postcss.plugin('easing-gradient', function easingGradient(options) {
-  return function (css) {
-    options = options || {};
-
-    css.walkRules(function (rule) {
-      rule.walkDecls(function (decl, i) {
-
+// TODO: Make eslint happy
+/* eslint-disable prefer-arrow-callback */
+module.exports = postcss.plugin('easing-gradient', function easingGradient(opts) {
+/* eslint-enable prefer-arrow-callback */
+  return function cssLoop(css) {
+    const options = opts || {};
+    css.walkRules((rule) => {
+      rule.walkDecls((decl) => {
         // If declarations value contains a supported gradient.
-        if (isEasingGradient(decl.value)) {
-
+        if (helpers.isEasingGradient(decl.value)) {
           // Parse the declaration and walk through the nodes.
           // https://github.com/TrySound/postcss-value-parser
           const parsedValue = valueParser(decl.value);
-          parsedValue.walk(function (node) {
-
+          parsedValue.walk((node) => {
             // Only do something to our gradient as the declaration can
             // contain more e.g. 'scrim-gradient(black, pink) center'
-            if (isEasingGradient(node.value)) {
-              let colors = [];
+            if (helpers.isEasingGradient(node.value)) {
+              const colors = [];
               let direction = '';
-
               // Loop through the easing gradient params and add colors to
               // colors array
-              for (let param of node.nodes) {
-
+              // TODO: Make eslint happy
+              /* eslint-disable no-restricted-syntax */
+              for (const param of node.nodes) {
                 // E.g. HSL colors gets parsed as functions so let's convert
                 // them back to words.
-                if (param.type === 'function') functionToWord(param);
-
+                if (param.type === 'function') helpers.functionToWord(param);
                 // If param is a color then add to colors array.
                 try {
-                  Color(param.value) && colors.push(param.value);
-                }
-
-                // If param isn't a color
-                catch(error) {
-
+                  Color(param.value);
+                  colors.push(param.value);
+                } catch (error) {
                   // Test if it's a word or space and before the first color
-                  // and assume that's part of the direction anotation e.g.
+                  // and assume that's part of the direction anotation e.g.
                   // 'to'+' '+'top' or '45deg'
                   if (param.type === 'word' || param.type === 'space') {
                     if (colors.length < 1) {
                       direction += param.value;
-
                     // But if it happens after the first color then it's prolly
                     // a color stop and not something we support
                     } else if (param.type !== 'space') {
@@ -83,24 +67,22 @@ postcss.plugin('easing-gradient', function easingGradient(options) {
                   }
                 }
               }
-
+              /* eslint-enable no-restricted-syntax */
               // Error handling: We only support exactly two colors
               if (colors.length !== 2) {
                 let errorMsg = `${errorPrefix} ${node.value} has `;
                 errorMsg += `${colors.length} valid colors — but only `;
-                errorMsg += `supports exactly 2`;
+                errorMsg += 'supports exactly 2';
                 console.log(errorMsg);
                 return;
               }
-
               // Error handling: Don't run if the two colors are identical
-              if (isSameColor(colors[0], colors[1])) {
+              if (helpers.isSameColor(colors[0], colors[1])) {
                 let errorMsg = `${errorPrefix} ${colors[0]} and ${colors[1]} `;
                 errorMsg += `in ${node.value} appear to be identical`;
                 console.log(errorMsg);
                 return;
               }
-
               // Get the color stops using our new
               const colorStops = helpers.getColorStops(
                 colors,
@@ -108,49 +90,19 @@ postcss.plugin('easing-gradient', function easingGradient(options) {
                 options.precision || defaultPrecision,
                 options.alphaDecimals || defaultAlphaDecimals
               );
-
               // Update node
               node.type = 'word';
-              node.value = `linear-gradient(${direction}${colorStops})`;
-
-              // Clean up if there is no direction
-              node.value = node.value.replace('gradient(, ', 'gradient(');
+              if (direction) {
+                node.value = `linear-gradient(${direction}, ${colorStops})`;
+              } else {
+                node.value = `linear-gradient(${colorStops})`;
+              }
             }
-          })
-
+          });
           // Update our declaration value
           decl.value = parsedValue.toString();
         }
       });
     });
-  }
+  };
 });
-
-/**
- * Check if a string matches one of the supported gradients
- */
-function isEasingGradient (str) {
-  return new RegExp(supportedGradients.join('|')).test(str);
-};
-
-/**
- * Convert a function object to a word object
- */
-function functionToWord(obj) {
-  let array = [];
-  for (let objNode of obj.nodes) {
-    if (objNode.type === 'word') {
-      array.push(objNode.value);
-    }
-  }
-  obj.value = `${obj.value}(${array.join()})`;
-  obj.type = 'word';
-  return obj;
-};
-
-/**
- * Test if two colors are identical
- */
-function isSameColor(colorA, colorB) {
-  return Color(colorA).hsl().string() === Color(colorB).hsl().string();
-};
